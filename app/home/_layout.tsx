c30e23c58457004a2f5cad6be8517e9a614517cf
z3r078 <kauanromero70@protonmail.com>
2024-10-04 21:13:07 -0300

    feat: Adding icon to index tab

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,9 +1,13 @@
 import { Tabs } from 'expo-router';
+import { Plus } from 'lucide-react-native';
 
 export default function HomeLayout() {
   return (    
     <Tabs>
-      <Tabs.Screen name="index" options={{ headerShown: false }} />
+      <Tabs.Screen name="index" options={{ 
+        headerShown: false,
+        tabBarIcon: (() => <Plus />)
+      }} />
     </Tabs>
   );
 }
