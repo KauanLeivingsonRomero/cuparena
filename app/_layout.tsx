4a5451603d4754cf6e26887f17611a6362761000
z3r078 <kauanromero70@protonmail.com>
2024-10-11 14:16:42 -0300

    feat: Create user Context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,17 +4,20 @@ import React from 'react';
 import 'react-native-reanimated';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import {ModalProvider} from "@/contexts/modalContext"
+import { UserProvider } from '@/contexts/userContext';
  
 export default function RootLayout() {
   
   return (
-    <ModalProvider>
-      <SafeAreaProvider>
-        <Stack screenOptions={{ headerStyle: { backgroundColor: COLORS.main }, headerTintColor: COLORS.main, animation: "none", headerShown: false}}>
-          <Stack.Screen name="index" options={{ headerShown: false, animation: 'none' }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false, animation: 'none' }} />
-        </Stack>
-      </SafeAreaProvider>
-    </ModalProvider>
+    <UserProvider>
+      <ModalProvider>
+        <SafeAreaProvider>
+          <Stack screenOptions={{ headerStyle: { backgroundColor: COLORS.main }, headerTintColor: COLORS.main, animation: "fade", headerShown: false}}>
+            <Stack.Screen name="index" options={{ headerShown: false, animation: 'none' }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false, animation: 'none' }} />
+          </Stack>
+        </SafeAreaProvider>
+      </ModalProvider>
+    </UserProvider>
   );
 }
---
+++ contexts/userContext.tsx
@@ -0,0 +1,45 @@
+import type { User, UserContextType } from '@/types/user';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { createContext, useContext, useEffect, useState, type ReactElement, type ReactNode } from 'react';
+
+const UserContext = createContext<UserContextType | undefined>(undefined);
+
+export const useUser = () => {
+  const context = useContext(UserContext)
+  if(!context){
+    throw new Error("useUser must be used within a UserProvider")
+  }
+  return context
+}
+
+const UserProvider = (props: {children: ReactNode}): ReactElement => {
+
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      const storedUser = await AsyncStorage.getItem("user")     
+      // console.log(storedUser) 
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    }
+    loadUser()
+  },[])
+
+  useEffect(() => {
+    if (user) {
+      AsyncStorage.setItem('user', JSON.stringify(user));
+    } else {
+      AsyncStorage.removeItem('user');
+    }
+  }, [user]);
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return <UserContext.Provider {...props} value={{ user, setUser, logout }}/>;
+}
+
+export  {UserContext,UserProvider } ;
