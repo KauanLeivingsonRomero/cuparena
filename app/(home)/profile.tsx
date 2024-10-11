9d8c84b70ea2a34fba5537c3a9fcc63374d6903a
z3r078 <kauanromero70@protonmail.com>
2024-10-11 16:49:37 -0300

    feat: Simple clear storage

--- app/(home)/profile.tsx
+++ app/(home)/profile.tsx
@@ -1,14 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useRouter } from 'expo-router';
+
 import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import { Button, ScrollView, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 // import { Container } from './styles';
 
 const Profile = () => {
+
+  const router = useRouter()
+
+  async function clearToken(){
+    await AsyncStorage.clear()
+    router.replace("/(auth)")
+    console.log("Clear storage")
+  }
+
   return(
     <SafeAreaView className='flex-1 flex'>
       <ScrollView>
-        <Text>Profile</Text>     
+        <Text>Profile</Text>
+        <Button title="clear storage" color={"red"} onPress={clearToken} />     
       </ScrollView>
     </SafeAreaView>
   );
