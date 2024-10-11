d1e7404f26ac2590f626938f82674d0b6cc156c0
z3r078 <kauanromero70@protonmail.com>
2024-10-11 14:14:44 -0300

    feat: Add main_black color

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
       colors: {
         "off-white": "#F5F5F5",
         "off-black": "#1E1E1E",
+        "main_black": "#1C1C1C",
         "gray": "#c9c9c9",
         "white": "#FFF",
         "green": "#46FF6F",
