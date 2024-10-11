24f472a6e3bd551f3f9afc77b4a21cc395c11ff0
z3r078 <kauanromero70@protonmail.com>
2024-10-11 16:49:51 -0300

    feat: Init home screen

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,3 +1,4 @@
+import { useUser } from '@/contexts/userContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { router } from 'expo-router';
 import React, { useEffect } from 'react';
@@ -5,17 +6,23 @@ import { Text, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
+
+  const {user} = useUser()
+  
   useEffect(() => {
-    const token = AsyncStorage.getItem("token")
-    if(!token){
-      router.replace("/(auth)")
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem("token")
+      if(!token){
+        router.replace("/(auth)")
+      }
     }
+    checkToken()
   },[])
 
   return(
     <SafeAreaView className='flex-1 flex'>
       <ScrollView>
-        <Text>Home</Text>     
+        <Text>Home {user?.name.split(" ")[0]}</Text>     
       </ScrollView>
     </SafeAreaView>
   );
