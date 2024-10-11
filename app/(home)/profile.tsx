c02d937d25d4718a86e477ac4492a5ce1e57a473
z3r078 <kauanromero70@protonmail.com>
2024-10-11 14:15:42 -0300

    feat: Improvised clearStorage

--- app/(home)/profile.tsx
+++ app/(home)/profile.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import { ScrollView, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from 'expo-router';
+import React, { useEffect } from 'react';
+import { Button, ScrollView, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 // import { Container } from './styles';
 
 const Profile = () => {
+
+  async function clearStorage(){
+    await AsyncStorage.clear()
+    .then(() => {
+      console.log("clear storage")
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    .finally(() => {
+      router.replace("/(auth)")
+    })
+  }
+
+  // useEffect(() => {clearStorage()},[])
+
   return(
     <SafeAreaView className='flex-1 flex'>
       <ScrollView>
-        <Text>Profile</Text>     
+        <Text>Profile</Text> 
+        <Button title='Clear token' color="#f00" onPress={clearStorage}/>    
       </ScrollView>
     </SafeAreaView>
   );
