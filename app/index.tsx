da26faf24c14191a14f22f3eeb4f2743ff0638bf
z3r078 <kauanromero70@protonmail.com>
2024-10-11 16:49:15 -0300

    fix: Load user while show splash screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import { useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as splash from 'expo-splash-screen';
-
+import { useUser } from '@/contexts/userContext';
 
 splash.preventAutoHideAsync();
 
 const SplashScreen = () => {
   const router = useRouter();
+  const { user, token, setUser, setToken } = useUser();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const checkAuth = async () => {
+    const loadUser = async () => {
       try {
+        const storedUser = await AsyncStorage.getItem("user");
+        const storedToken = await AsyncStorage.getItem("token");
 
-        const token = await AsyncStorage.getItem('token');
-        
-        if (token) {
-          router.replace("/(home)");
+        if (storedUser) {
+          setUser(JSON.parse(storedUser));
+          setToken(storedToken);
+        }
+
+        if (storedToken && storedUser) {
+          router.replace("/(home)");  // Redirect to home if both token and user are available
         } else {
-          router.replace('/(auth)');
+          router.replace("/(auth)");  // Redirect to login if token or user is missing
         }
       } catch (error) {
-        console.error('Erro ao verificar token:', error);
-        router.replace('/(auth)');
-      } finally{
+        console.error("Error loading user data", error);
+        router.replace("/(auth)");  // Redirect to login on error
+      } finally {
+        setLoading(false);  // Set loading to false once user data is loaded
         splash.hideAsync();
       }
     };
 
-    checkAuth();
+    loadUser();
   }, []);
+
+  if (loading) {
+    return null;  // Return null while loading
+  }
 
   return (
     <View className='flex-1 flex justify-center align-items bg-[#1c1c1c]'>
-
+      {/* You can add a loading spinner here if you'd like */}
     </View>
   );
 };
